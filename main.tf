d97164df9a3c1a4193332c2fc958dca73a2e6121
Sush <sushcloud12@gmail.com>
2023-11-09 00:30:13 +0000

    i

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ module "redis" {
     source = "./vendor/modules/redis"   # path created after running terrfile -f command 
     ENV = var.ENV
 }
+
+module "mysql" {
+    source = "./vendor/modules/mysql"   # path created after running terrfile -f command 
+    ENV = var.ENV
+}
