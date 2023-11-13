3b1993862cadf133dcc0006462b75ee7bfc770d5
Sush <sushcloud12@gmail.com>
2023-11-13 16:59:06 +0000

    i

--- main.tf
+++ main.tf
@@ -4,11 +4,16 @@ module "docdb" {
     # DOCDB variables
     DOCDB_INSTANCE_COUNT = var.DOCDB_INSTANCE_COUNT
     DOCDB_INSTANCE_TYPE = var.DOCDB_INSTANCE_TYPE
+    DOCDB_INSTANCE_PORT = var.DOCDB_INSTANCE_PORT
 }
 
 module "redis" {
     source = "./vendor/modules/redis"   # path created after running terrfile -f command 
     ENV = var.ENV
+    REDIS_ENGINE_VERSION = var.REDIS_ENGINE_VERSION
+    REDIS_INSTANCE_TYPE = var.REDIS_INSTANCE_TYPE
+    REDIS_PORT = var.REDIS_PORT
+    REDIS_INSTANCE_COUNT = var.REDIS_INSTANCE_COUNT
 }
 
 module "mysql" {
@@ -18,3 +23,7 @@ module "mysql" {
     MYSQL_ENGINE_VERSION = var.MYSQL_ENGINE_VERSION
     MYSQL_INSTANCE_TYPE = var.MYSQL_INSTANCE_TYPE
 }
+
+
+
+
--- vars.tf
+++ vars.tf
@@ -8,5 +8,13 @@ variable "MYSQL_INSTANCE_TYPE" {}
 # DOCDB variables
 variable "DOCDB_INSTANCE_COUNT" {}
 variable "DOCDB_INSTANCE_TYPE" {}
+variable "DOCDB_INSTANCE_PORT" {}
 
+
+
+# Redis variables
+variable "REDIS_ENGINE_VERSION" {}
+variable "REDIS_INSTANCE_TYPE" {}
+variable "REDIS_PORT" {}
+variable "REDIS_INSTANCE_COUNT" {}
 
